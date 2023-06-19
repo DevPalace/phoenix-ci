fdf4b54de06a270be536052ca476a6e2f713e3bb
Gytis Ivaskevicius <me@gytis.io>
2023-06-19 23:53:19 +0300

    chore: update dist

--- dist/index.js
+++ dist/index.js
@@ -426,6 +426,7 @@ const worker_1 = __nccwpck_require__(7399);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         const type = core.getInput('type');
+        console.log(process.env);
         try {
             if (type === 'discovery') {
                 yield (0, discovery_1.runDiscovery)();
